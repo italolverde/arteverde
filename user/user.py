a60acefc892bba116a869e47f7cda348275d5452
Ítalo Lima Verde <125528153+italolverde@users.noreply.github.com>
2024-12-09 17:25:54 +0000

    nada dmais

--- user/user.py
+++ user/user.py
@@ -2,22 +2,15 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, login_required, login_user, logout_user, UserMixin
 import sqlite3
 
-##
-
 login_manager = LoginManager()
-app.config['SECRET_KEY'] = 'CHAVE'
-login_manager.init_app(app)
-
-##
 
 def get_connection():
     conn = sqlite3.connect("../banco.db")
     conn.row_factory = sqlite3.Row
     return conn
 
-##
 
-class User(UserMixin)
+class User(UserMixin):
     def __init__(self,email,nome,senha):
         self.email = email
         self.nome = nome
